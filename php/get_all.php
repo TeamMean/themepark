974376d73f86ca6826f923f8e36ea4ee54838ce5
James C <james@newleafgs.com>
2016-08-08 20:41:10 +0000

    Added the get_all.php script

---
+++ php/get_all.php
@@ -0,0 +1,61 @@
+<?php
+  $conf_arr = parse_ini_file("database.ini");
+  $con = new mysqli("localhost", $conf_arr['user'], $conf_arr['password']);
+  echo $con->error;
+
+  $con->query("USE ".$conf_arr['database']);
+
+  $collections = [];
+
+  //Temporary storage variable
+  $collection_curr = [];
+
+  //For storing the themeparks
+  $collection_tp = [];
+  //For storing the tickets
+  $collection_tk = [];
+  //For storing the attractions
+  $collection_at = [];
+  //For storing the hours
+  $collection_hr = [];
+  //For storing the employees
+  $collection_em = [];
+
+  //Getting the themeparks
+  $res = $con->query("SELECT * FROM themepark;");
+  while($row = $res->fetch_assoc())
+  {
+    $collection_curr[] = $row;
+  }
+
+  $collection_tp['name'] = "themepark";
+  $collection_tp['data'] = $collection_curr;
+
+  //Getting the ticket data  
+  $collection_curr = [];
+  $res = $con->query("SELECT * FROM ticket LEFT JOIN themepark ON ticket.park_code = themepark.park_code;");
+  while($row = $res->fetch_assoc())
+  {
+    $collection_curr[] = $row;
+  }
+
+  $collection_tk['name'] = "ticket";
+  $collection_tk['data'] = $collection_curr;
+
+  //Getting the attraction data
+  $collection_curr = [];
+  $res = $con->query("SELECT * FROM attraction LEFT JOIN themepark ON attraction.park_code = themepark.park_code;");
+  while($row = $res->fetch_assoc())
+  {
+    $collection_curr[] = $row;
+  }
+
+  $collection_at['name'] = "attraction";
+  $collection_at['data'] = $collection_curr;
+
+  $collections[] = $collection_tp;
+  $collections[] = $collection_tk;
+  $collections[] = $collection_at;
+
+  echo json_encode($collections);
+?>
--- php/install.php
+++ php/install.php
@@ -72,21 +72,25 @@
     CONSTRAINT	FK_hours_attract FOREIGN KEY (attract_no) REFERENCES attraction(attract_no))
   ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_bin;";
   $con->query($query);
-  echo $con->error;
+
+  //Create the individual themeparks
   $query = "INSERT INTO themepark VALUES ('FR1001','FairyLand','PARIS','FR'), ('NL1202','Efling','NOORD','NL'), ('SP4533','AdventurePort','BARCELONA','SP'), ('SW2323','Labyrinthe','LAUSANNE','SW'), ('UK2622','MiniLand','WINDSOR','UK'), ('UK3452','PleasureLand','STOKE','UK'), ('ZA1342','GoldTown','JOHANNESBURG','ZA');";
   $con->query($query);
-  echo $con->error;
-  
+
+  //Create the employees
   $query = "INSERT INTO employee VALUES (100,'Ms','Calderdale','Emma',STR_TO_DATE('15-Jun-72','%d-%b-%Y'),STR_TO_DATE('15-Mar-92','%d-%b-%Y'),'0181','324-9134', '".hash('sha512', 'password')."', 1), (101,'Ms','Ricardo','Marshel',STR_TO_DATE('19-Mar-78','%d-%b-%Y'),STR_TO_DATE('25-Apr-96','%d-%b-%Y'),'0181','324-4472', '".hash('sha512', 'password')."', 1), (102,'Mr','Arshad','Arif',STR_TO_DATE('14-Nov-69','%d-%b-%Y'),STR_TO_DATE('20-Dec-90','%d-%b-%Y'),'7253','675-8993', '".hash('sha512', 'password')."', 1), (103,'Ms','Roberts','Anne',STR_TO_DATE('16-Oct-74','%d-%b-%Y'),STR_TO_DATE('16-Aug-94','%d-%b-%Y'),'0181','898-3456', '".hash('sha512', 'password')."', 1), (104,'Mr','Denver','Enrica',STR_TO_DATE('08-Nov-80','%d-%b-%Y'),STR_TO_DATE('20-Oct-01','%d-%b-%Y'),'7253','504-4434', '".hash('sha512', 'password')."', 1), (105,'Ms','Namowa','Mirrelle',STR_TO_DATE('14-Mar-90','%d-%b-%Y'),STR_TO_DATE('08-Nov-06','%d-%b-%Y'),'0181','890-3243', '".hash('sha512', 'password')."', 1), (106,'Mrs','Smith','Gemma',STR_TO_DATE('12-Feb-68','%d-%b-%Y'),STR_TO_DATE('05-Jan-89','%d-%b-%Y'),'0181','324-7845', '".hash('sha512', 'password')."', 1);";
   $con->query($query);
 
+  //Create the tickets
   $query = "INSERT INTO ticket VALUES (4668,24.99,'Adult','SP4533'), (13001,14.99,'Child','FR1001'), (13002,34.99,'Adult','FR1001'), (13003,34.99,'Adult','FR1001'), (18721,14.99,'Child','FR1001'), (18722,14.99,'Child','FR1001'), (18723,20.99,'Senior','FR1001'), (18724,34.99,'Adult','FR1001'), (32450,24.99,'Adult','SP4533'), (45767,24.99,'Adult','SP4533'), (67832,18.56,'Child','ZA1342'), (67833,28.67,'Adult','ZA1342'), (67855,18.56,'Child','ZA1342'), (88567,22.50,'Child','UK3452'), (88568,42.10,'Adult','UK3452'), (89720,22.50,'Child','UK3452'), (89723,22.50,'Child','UK3452'), (89725,22.50,'Child','UK3452'), (89728,42.10,'Adult','UK3452')";
   $con->query($query);
 
-  $query = "";
+  //Create the attractions
+  $query = "INSERT INTO attraction VALUES (10034,'ThunderCoaster',11,34,'FR1001'), (10056,'SpinningTeacups',4,62,'FR1001'), (10067,'FlightToStars',11,24,'FR1001'), (10078,'Ant-Trap',23,30,'FR1001'), (10098,'Carnival',3,120,'FR1001'), (20056,'3D-Lego_Show',3,200,'UK2622'), (30011,'BlackHole2',12,34,'UK3452'), (30012,'Pirates',10,42,'UK3452'), (30044,'UnderSeaWord',4,80,'UK3452'), (98764,'GoldRush',5,80,'ZA1342');";
   $con->query($query);
 
-  $query = "";
+  //Create the Jobs
+  $query = "INSERT INTO hours VALUES (100,10034,6,6.5,STR_TO_DATE('18-May-2007','%d-%b-%Y')), (100,10034,6,6.5,STR_TO_DATE('20-May-2007','%d-%b-%Y')), (101,10034,6,6.5,STR_TO_DATE('18-May-2007','%d-%b-%Y')), (102,30012,3,5.99,STR_TO_DATE('23-May-2007','%d-%b-%Y')), (102,30044,6,5.99,STR_TO_DATE('22-May-2007','%d-%b-%Y')), (102,30044,3,5.99,STR_TO_DATE('23-May-2007','%d-%b-%Y')), (104,30011,6,7.2,STR_TO_DATE('21-May-2007','%d-%b-%Y')), (104,30012,6,7.2,STR_TO_DATE('22-May-2007','%d-%b-%Y')), (105,10078,3,8.5,STR_TO_DATE('18-May-2007','%d-%b-%Y')), (105,10098,3,8.5,STR_TO_DATE('18-May-2007','%d-%b-%Y')), (105,10098,6,8.5,STR_TO_DATE('19-May-2007','%d-%b-%Y'));";
   $con->query($query);
 
 

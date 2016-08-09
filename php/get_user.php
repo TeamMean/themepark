f728eb6c50ee67f00d79b4d3d97eb84853ee3d54
James C <james@newleafgs.com>
2016-08-09 13:36:18 +0000

    Added a logout page

--- php/get_user.php
+++ php/get_user.php
@@ -3,64 +3,66 @@
   if(!isset($_SESSION['emp_num']) || !isset($_SESSION['password']))
   {
     echo json_encode([]);
-    die();
   }
-  $conf_arr = parse_ini_file("../../database.ini");
-  $con = new mysqli("localhost", $conf_arr['user'], $conf_arr['password']);
-  echo $con->error;
-
-  $con->query("USE ".$conf_arr['database']);
-
-  $collections = [];
-
-  //Temporary storage variable
-  $collection_curr = [];
-
-  //For storing the tickets
-  $collection_tk = [];
-  //For storing the hours
-  $collection_hr = [];
-  //For storing the employees
-  $collection_em = [];
-
-  //Getting the ticket data  
-  $collection_curr = [];
-  $res = $con->query("SELECT * FROM ticket LEFT JOIN themepark ON ticket.park_code = themepark.park_code LEFT JOIN employee ON employee.emp_num = ticket.emp_num WHERE ticket.emp_num=".$_SESSION['emp_num'].";");
-  while($row = $res->fetch_assoc())
+  else
   {
-    $collection_curr[] = $row;
-  }
+    $conf_arr = parse_ini_file("../../database.ini");
+    $con = new mysqli("localhost", $conf_arr['user'], $conf_arr['password']);
+    echo $con->error;
 
-  $collection_tk['name'] = "ticket";
-  $collection_tk['data'] = $collection_curr;
+    $con->query("USE ".$conf_arr['database']);
 
-  //Getting all hours data
-  $collection_curr = [];
-  $res = $con->query("SELECT * FROM hours JOIN employee ON employee.emp_num = hours.emp_num JOIN attraction ON attraction.attract_no = hours.attract_no JOIN themepark ON themepark.park_code = attraction.park_code WHERE employee.emp_num = ".$_SESSION['emp_num'].";");
-  while($row = $res->fetch_assoc())
-  {
-    $row['emp_passwd'] = "";
-    $collection_curr[] = $row;
-  }
+    $collections = [];
 
-  $collection_hr['name'] = "hours";
-  $collection_hr['data'] = $collection_curr;
+    //Temporary storage variable
+    $collection_curr = [];
 
-  //Getting all employees
-  $collection_curr = [];
-  $res = $con->query("SELECT * FROM employee WHERE employee.emp_num = ".$_SESSION['emp_num'].";");
-  while($row = $res->fetch_assoc())
-  {
-    $row['emp_passwd'] = "";
-    $collection_curr[] = $row;
-  }
+    //For storing the tickets
+    $collection_tk = [];
+    //For storing the hours
+    $collection_hr = [];
+    //For storing the employees
+    $collection_em = [];
 
-  $collection_em['name'] = "employee";
-  $collection_em['data'] = $collection_curr;
+    //Getting the ticket data  
+    $collection_curr = [];
+    $res = $con->query("SELECT * FROM ticket LEFT JOIN themepark ON ticket.park_code = themepark.park_code LEFT JOIN employee ON employee.emp_num = ticket.emp_num WHERE ticket.emp_num=".$_SESSION['emp_num'].";");
+    while($row = $res->fetch_assoc())
+    {
+      $collection_curr[] = $row;
+    }
 
-  $collections[] = $collection_tk;
-  $collections[] = $collection_hr;
-  $collections[] = $collection_em;
+    $collection_tk['name'] = "ticket";
+    $collection_tk['data'] = $collection_curr;
+
+    //Getting all hours data
+    $collection_curr = [];
+    $res = $con->query("SELECT * FROM hours JOIN employee ON employee.emp_num = hours.emp_num JOIN attraction ON attraction.attract_no = hours.attract_no JOIN themepark ON themepark.park_code = attraction.park_code WHERE employee.emp_num = ".$_SESSION['emp_num'].";");
+    while($row = $res->fetch_assoc())
+    {
+      $row['emp_passwd'] = "";
+      $collection_curr[] = $row;
+    }
+
+    $collection_hr['name'] = "hours";
+    $collection_hr['data'] = $collection_curr;
+
+    //Getting all employees
+    $collection_curr = [];
+    $res = $con->query("SELECT * FROM employee WHERE employee.emp_num = ".$_SESSION['emp_num'].";");
+    while($row = $res->fetch_assoc())
+    {
+      $row['emp_passwd'] = "";
+      $collection_curr[] = $row;
+    }
+
+    $collection_em['name'] = "employee";
+    $collection_em['data'] = $collection_curr;
+
+    $collections[] = $collection_tk;
+    $collections[] = $collection_hr;
+    $collections[] = $collection_em;
   
-  echo json_encode($collections);
+    echo json_encode($collections);
+  }
 ?>
--- php/login.php
+++ php/login.php
@@ -1,9 +1,13 @@
 <?php
   session_start();
+  $_SESSION['emp_num'] = 101;
+  $_SESSION['password'] = "abc";
 //  if(isset($_SESSION['emp_num']) && isset($_SESSION['password']))
 //  {
     echo "{ \"all_data\":";
     require("get_all.php");
+    echo ", \"user_data\":";
+    require("get_user.php");
     echo "}";
 /*  }
   else
@@ -16,5 +20,5 @@
     {
       echo "You are not authorized to see this.";
     }
-  }* 
+  }*/ 
 ?>
